6145a664a3cd0e2acf974a15bc6b11b96ae1b211
pedro-tramontin <42963689+pedro-tramontin@users.noreply.github.com>
2020-01-13 11:30:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('Loading function');
 
 exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
-    event.body.version = 2;
-    return { "isBase64Encoded": false, "statusCode": 200, "body": event.body };  // Echo back the first key value
+//     event.body.version = 2;
+    return { "isBase64Encoded": false, "statusCode": 200, "body": 3 };  // Echo back the first key value
     // throw new Error('Something went wrong');
 };
